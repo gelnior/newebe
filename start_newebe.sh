edbed4ef2820e1620e71da54de7d36a9490b798e
Gelnior <gelnior@free.fr>
2010-10-19 00:00:14 +0000

    Maj fichier settings

--- start_newebe.sh
+++ start_newebe.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-# Replace these three settings.
 PROJDIR="/home/newebe/Newebe/"
 PIDFILE="$PROJDIR/newebe.pid"
 SOCKET="$PROJDIR/newebe.sock"
 
+# Remove old PID.
 cd $PROJDIR
 if [ -f $PIDFILE ]; then
     kill `cat -- $PIDFILE`
     rm -f -- $PIDFILE
 fi
 
+# Set up a new django fast-cgi socket.
 exec /usr/bin/env - \
   PYTHONPATH="../python:.." \
   python manage.py runfcgi socket=$SOCKET pidfile=$PIDFILE
 
-chmod 777 $SOCKET
+# Add permissions on socket for allowing lighttpd to use it.
+chmod 770 $SOCKET
