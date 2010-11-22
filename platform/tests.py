65595c59303c229d845c424c8cb620447e29d937
gelnior <gelnior@sommerless.(none)>
2010-11-23 00:11:34 +0100

    first test on platform

---
+++ platform/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+
+from newebe.lib import date_util
+
+class DateUtilTest(TestCase):
+     def testGetDbDateFromUrlDate(self):
+         '''
+         Tests if correct format is returned from function.
+         '''
+         expectedDate = "2010-10-01T11:05:12Z"
+         dateToTest = "2010-10-01-11-05-12"
+         self.assertEqual(expectedDate, 
+                          date_util.getDbDateFromUrlDate(dateToTest))
+
+class FileUtilTest(TestCase):
+     def testStaticFileResponse(self):
+         pass
+
+
+class JsonUtilTest(TestCase):
+    def testGetJsonFromList(self):    
+         pass 
+         # TODO when platform will have docs inside its model
