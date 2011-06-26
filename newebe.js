31fad7b5bf7dae3809d33490e3ff50ea9e94ac4d
gelnior <gelnior@free.fr>
2011-06-27 00:21:36 +0200

    set js file in index.html

--- newebe.js
+++ newebe.js
@@ -1,57 +1,57 @@
-$(function(){
+(function() {
+  $(function() {
+    var lastSelected, lastSelectedMenu;
     $('#installation').hide();
     $('#documentation').hide();
     $('#contact').hide();
     $('#developers').hide();
-    var lastSelected = $('#home');
-    var lastSelectedMenu = $('#menu-home');
+    lastSelected = $('#home');
+    lastSelectedMenu = $('#menu-home');
     lastSelectedMenu.addClass('selected');
-
     $('#menu-home').click(function(event) {
-        lastSelected.fadeOut(300, function() {
-            $('#home').fadeIn(500);
-        });
-        lastSelected = $('#home');
-        lastSelectedMenu.removeClass('selected');
-        lastSelectedMenu = $('#menu-home');
-        lastSelectedMenu.addClass('selected');
+      lastSelected.fadeOut(300, function() {
+        return $('#home').fadeIn(500);
+      });
+      lastSelected = $('#home');
+      lastSelectedMenu.removeClass('selected');
+      lastSelectedMenu = $('#menu-home');
+      return lastSelectedMenu.addClass('selected');
     });
     $('#menu-installation').click(function(event) {
-        lastSelected.fadeOut(300, function() {
-            $('#installation').fadeIn(500);
-        });
-        lastSelected = $('#installation');
-        lastSelectedMenu.removeClass('selected');
-        lastSelectedMenu = $('#menu-installation');
-        lastSelectedMenu.addClass('selected');
+      lastSelected.fadeOut(300, function() {
+        return $('#installation').fadeIn(500);
+      });
+      lastSelected = $('#installation');
+      lastSelectedMenu.removeClass('selected');
+      lastSelectedMenu = $('#menu-installation');
+      return lastSelectedMenu.addClass('selected');
     });
     $('#menu-documentation').click(function(event) {
-        lastSelected.fadeOut(300, function() {
-            $('#documentation').fadeIn(300);
-        });
-        lastSelected = $('#documentation');
-        lastSelectedMenu.removeClass('selected');
-        lastSelectedMenu = $('#menu-documentation');
-        lastSelectedMenu.addClass('selected');
+      lastSelected.fadeOut(300, function() {
+        return $('#documentation').fadeIn(300);
+      });
+      lastSelected = $('#documentation');
+      lastSelectedMenu.removeClass('selected');
+      lastSelectedMenu = $('#menu-documentation');
+      return lastSelectedMenu.addClass('selected');
     });
     $('#menu-contact').click(function(event) {
-        lastSelected.fadeOut(300, function() {
-            $('#contact').fadeIn(300);
-        });
-        lastSelected = $('#contact');
-        lastSelectedMenu.removeClass('selected');
-        lastSelectedMenu = $('#menu-contact');
-        lastSelectedMenu.addClass('selected');
+      lastSelected.fadeOut(300, function() {
+        return $('#contact').fadeIn(300);
+      });
+      lastSelected = $('#contact');
+      lastSelectedMenu.removeClass('selected');
+      lastSelectedMenu = $('#menu-contact');
+      return lastSelectedMenu.addClass('selected');
     });
-    $('#menu-developers').click(function(event) {
-        lastSelected.fadeOut(300, function() {
-            $('#developers').fadeIn(300);
-        });
-        lastSelected = $('#developers');
-        lastSelectedMenu.removeClass('selected');
-        lastSelectedMenu = $('#menu-developers');
-        lastSelectedMenu.addClass('selected');
+    return $('#menu-developers').click(function(event) {
+      lastSelected.fadeOut(300, function() {
+        return $('#developers').fadeIn(300);
+      });
+      lastSelected = $('#developers');
+      lastSelectedMenu.removeClass('selected');
+      lastSelectedMenu = $('#menu-developers');
+      return lastSelectedMenu.addClass('selected');
     });
-});
-
-
+  });
+}).call(this);
