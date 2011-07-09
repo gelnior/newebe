1989a582feea85f7d963d609719e500eee5813f9
gelnior <gelnior@free.fr>
2011-07-09 18:40:10 +0200

    add slugify tool from django

---
+++ lib/slugify.py
@@ -0,0 +1,12 @@
+import unicodedata
+import re
+
+def slugify(value):
+    """
+    Normalizes string, converts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+    """
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
+    return re.sub('[-\s]+', '-', value)
+
