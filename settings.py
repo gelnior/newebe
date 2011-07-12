bdd01a9411b238359583c7a375c777a4807f0ce5
gelnior <gelnior@free.fr>
2011-07-13 00:38:13 +0200

    settings cleaning

--- settings.py
+++ settings.py
@@ -3,9 +3,6 @@ TORNADO_PORT = 8000
 COUCHDB_DB_NAME = "newebe"
 DEBUG = False
 
-# Main variables
-SECRET_KEY = '*=$m*%d4u8gi$ry8#_ez&**3s#8wtv8x^a3_tdrk$snhd-uw_a'
-
 # Couchdb configuration
 COUCHDB_DATABASES = (
     ('newebe.news', 'http://127.0.0.1:5984/%s' % COUCHDB_DB_NAME),
@@ -21,32 +18,3 @@ INSTALLED_APPS = (
     'newebe.notes',
 )
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
-        'simple': {
-            'format': '[%(levelname)s] %(asctime)s: %(message)s'
-        },
-    },
-    'handlers': {
-        'null': {
-            'level':'DEBUG',
-            'class':'django.utils.log.NullHandler',
-        },
-        'console':{
-            'level':'DEBUG',
-            'class':'logging.StreamHandler',
-            'formatter': 'simple'
-        }
-    },
-    'loggers': {
-        'newebe': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        }
-    }
-}
