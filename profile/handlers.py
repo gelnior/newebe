80aae4db0e0a4c7efd9eea8a6b504ecebfa05f11
gelnior <gelnior@free.fr>
2011-08-22 23:04:27 +0200

    move profile template handlers to profile module

--- profile/handlers.py
+++ profile/handlers.py
@@ -2,6 +2,14 @@ from newebe.core.handlers import NewebeAuthHandler
 
 # Template handlers.
 
+class ProfileContentTHandler(NewebeAuthHandler):
+    def get(self):
+        self.render("templates/profile_content.html")
+
+class ProfileMenuContentTHandler(NewebeAuthHandler):
+    def get(self):
+        self.render("templates/profile_menu_content.html")
+
 class ProfileTHandler(NewebeAuthHandler):
     def get(self):
         self.render("templates/profile.html")
@@ -9,4 +17,13 @@ class ProfileTHandler(NewebeAuthHandler):
 class ProfileMenuContentTHandler(NewebeAuthHandler):
     def get(self):
         self.render("templates/profile_menu_content.html")
+
+class ProfileTutorial1THandler(NewebeAuthHandler):
+    def get(self):
+        self.render("templates/tutorial_1.html")
+
+class ProfileTutorial2THandler(NewebeAuthHandler):
+    def get(self):
+        self.render("templates/tutorial_2.html")
+
 
