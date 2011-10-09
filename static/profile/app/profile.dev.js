5a956053e9c442f52b890473cc36e5a7710e0315
gelnior <gelnior@free.fr>
2011-10-09 15:07:30 +0200

    add call to show password dialog

--- static/profile/app/profile.dev.js
+++ static/profile/app/profile.dev.js
@@ -1,118 +1,20 @@
 (function() {
-<<<<<<< HEAD
-  var FormDialog, ProfileView, User, UserCollection, profileApp;
-  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
-=======
   var ProfileView, User, UserCollection, profileApp;
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
->>>>>>> 5f3d1060a7e5a7bd622367cdccf5b21b7eb94af2
     for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
     function ctor() { this.constructor = child; }
     ctor.prototype = parent.prototype;
     child.prototype = new ctor;
     child.__super__ = parent.prototype;
     return child;
-<<<<<<< HEAD
-  }, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-  FormDialog = (function() {
-    function FormDialog() {
-      var div;
-      if ($("#form-dialog").length === 0) {
-        div = document.createElement('div');
-        div.id = "form-dialog";
-        div.className = "dialog";
-        $("body").prepend(div);
-        this.element = $("#form-dialog");
-        this.element.html('<div id="form-dialog-text"></div>\n<div id="form-dialog-buttons">\'\n  <span id="form-dialog-yes">Yes</span>\n  <span id="form-dialog-no">No</span>\n</div>');
-      }
-      this.element = $("#form-dialog");
-      this.element.hide();
-      this.fields = [];
-    }
-    FormDialog.prototype.addField = function(field) {
-      return this.fields.append(field);
-    };
-    FormDialog.prototype.clearFields = function() {
-      return this.fields = [];
-    };
-    FormDialog.prototype.setNoButton = function() {
-      var divElement;
-      divElement = this.element;
-      return $("#confirmation-no").click(function() {
-        divElement.fadeOut();
-        return false;
-      });
-    };
-    FormDialog.prototype.display = function(text, callback) {
-      $("#confirmation-text").empty();
-      $("#confirmation-text").append('<span>' + text + '</span>');
-      $("#confirmation-yes").click(callback);
-      return this.element.show();
-    };
-    FormDialog.prototype.hide = function() {
-      return this.element.fadeOut();
-    };
-    return FormDialog;
-  })();
-  User = (function() {
-    __extends(User, Backbone.Model);
-    User.prototype.url = '/user/';
-    function User(user) {
-      User.__super__.constructor.apply(this, arguments);
-      this.id = "";
-      this.set("url", user.url);
-      this.set("name", user.name);
-      this.set("description", user.description);
-    }
-    /* Setters / Accessors */
-    User.prototype.getName = function() {
-      return this.get("name");
-    };
-    User.prototype.setName = function(name) {
-      alert(name);
-      this.set("name", name);
-      return alert(this.getName());
-    };
-    User.prototype.getUrl = function() {
-      return this.get("userUrl");
-    };
-    User.prototype.setUrl = function(url) {
-      return this.set("url", url);
-    };
-    User.prototype.getDescription = function() {
-      return this.get("description");
-    };
-    User.prototype.setDescription = function(description) {
-      return this.set("description", description);
-    };
-    User.prototype.isNew = function() {
-      return false;
-    };
-    return User;
-  })();
-  /* Model for a User collection */
-  UserCollection = (function() {
-    function UserCollection() {
-      UserCollection.__super__.constructor.apply(this, arguments);
-    }
-    __extends(UserCollection, Backbone.Collection);
-    UserCollection.prototype.model = User;
-    UserCollection.prototype.url = '/user/';
-    UserCollection.prototype.parse = function(response) {
-      return response.rows;
-    };
-    return UserCollection;
-  })();
-=======
   };
->>>>>>> 5f3d1060a7e5a7bd622367cdccf5b21b7eb94af2
   ProfileView = (function() {
     __extends(ProfileView, Backbone.View);
     ProfileView.prototype.el = $("#profile");
     function ProfileView() {
-      this.onDescriptionEditClicked = __bind(this.onDescriptionEditClicked, this);
-      this.onMouseOut = __bind(this.onMouseOut, this);
-      this.onMouseOver = __bind(this.onMouseOver, this);      ProfileView.__super__.constructor.call(this);
+      this.onDescriptionEditClicked = __bind(this.onDescriptionEditClicked, this);;
+      this.onMouseOut = __bind(this.onMouseOut, this);;
+      this.onMouseOver = __bind(this.onMouseOver, this);;      ProfileView.__super__.constructor.call(this);
     }
     ProfileView.prototype.initialize = function() {
       _.bindAll(this, 'onKeyUp', 'postUserInfo', 'fetch', 'addAll');
@@ -123,6 +25,7 @@
     /* Events */
     ProfileView.prototype.events = {
       "click #profile-description-edit": "onDescriptionEditClicked",
+      "click #change-password(": "onDescriptionEditClicked",
       "mouseover #profile div.app": "onMouseOver",
       "mouseout #profile div.app": "onMouseOut"
     };
@@ -283,10 +186,10 @@
   })();
   /* Model for a User collection */
   UserCollection = (function() {
-    __extends(UserCollection, Backbone.Collection);
     function UserCollection() {
       UserCollection.__super__.constructor.apply(this, arguments);
     }
+    __extends(UserCollection, Backbone.Collection);
     UserCollection.prototype.model = User;
     UserCollection.prototype.url = '/user/';
     UserCollection.prototype.parse = function(response) {
