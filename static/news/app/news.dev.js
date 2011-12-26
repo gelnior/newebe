a29f94c053944ab79ffa368ec4a1d5f5353d1a89
gelnior <gelnior@free.fr>
2011-12-26 20:08:56 +0100

    debug micropost posting

--- static/news/app/news.dev.js
+++ static/news/app/news.dev.js
@@ -466,11 +466,13 @@
       var regexp, url, urlIndex, urls, _i, _len;
       regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/g;
       urls = content.match(regexp);
-      for (_i = 0, _len = urls.length; _i < _len; _i++) {
-        url = urls[_i];
-        urlIndex = content.indexOf(url);
-        if (urlIndex === 0 || content.charAt(urlIndex - 1) !== '(') {
-          content = content.replace(url, "[" + url + "]" + "(" + url + ")");
+      if (urls) {
+        for (_i = 0, _len = urls.length; _i < _len; _i++) {
+          url = urls[_i];
+          urlIndex = content.indexOf(url);
+          if (urlIndex === 0 || content.charAt(urlIndex - 1) !== '(') {
+            content = content.replace(url, "[" + url + "]" + "(" + url + ")");
+          }
         }
       }
       return content;
--- static/news/src/views/newsview.coffee
+++ static/news/src/views/newsview.coffee
@@ -249,9 +249,11 @@ class NewsView extends Backbone.View
     regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/g
 
     urls = content.match(regexp)
-    for url in urls
-      urlIndex = content.indexOf(url)
-      if urlIndex == 0 or content.charAt(urlIndex - 1) != '('
+
+    if urls
+     for url in urls
+       urlIndex = content.indexOf(url)
+       if urlIndex == 0 or content.charAt(urlIndex - 1) != '('
         content = content.replace(url, "[" + url + "]" + "(" + url + ")" )
     content
 
