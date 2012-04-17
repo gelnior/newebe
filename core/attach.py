72165f43c5d7fd2e7ab7ed50d85c356de59f9f0d
Gelnior <gelnior@free.fr>
2012-04-18 00:46:24 +0200

    add comments

--- core/attach.py
+++ core/attach.py
@@ -1,8 +1,18 @@
 from newebe.notes.models import NoteManager
 
 class Converter():
+    '''
+    Convert data send by user as attachment to real data to send to newebe
+    contacts.
+    '''
 
     def convert(self, data):
+        '''
+        Expect to have an attachments field in given dict. When dict has some
+        attachments, it retrieves corresponding docs and convert them in
+        attachment dict (same as usual dict with less fields).
+        Then attach docs are returned inside an array.
+        '''
 
         docs = []
         for doc in data.get("attachments", []):
