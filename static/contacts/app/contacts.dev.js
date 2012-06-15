9024647703aeba136d0eeb6a860fe0aa209fcf26
Gelnior <gelnior@free.fr>
2012-06-15 02:43:48 +0200

    remove smull contact bugs

--- static/contacts/app/contacts.dev.js
+++ static/contacts/app/contacts.dev.js
@@ -228,7 +228,7 @@
   String.prototype.isNewebeUrl = function() {
     var regexp;
     regexp = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/g;
-    return this.match(regexp) !== null;
+    return this.match(regexp) !== null || this.length === 0;
   };
 
   ContactView = (function(_super) {
