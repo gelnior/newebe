01657e98a55aff1cfb7ab445ba0ed4f7d5c853f9
Gelnior <gelnior@free.fr>
2012-07-22 22:33:26 +0200

    add utility methods to manage views based on tags

--- core/models.py
+++ core/models.py
@@ -112,7 +112,7 @@ class DocumentManager():
 
     @staticmethod
     def get_documents(docType, view, startKey=None, endKey=None, 
-                      skip=0, limit=10, group=True):
+                      skip=0, limit=10, group=False):
 
         '''
         Returns documents of which type is *docType* from given *view*.
@@ -131,8 +131,17 @@ class DocumentManager():
                                  descending = True, 
                                  limit = limit+1,
                                  endKey = endKey, 
+                                 skip = 0)
+        elif group:
+            documents = docType.view(view, 
+                                 startkey = startKey, 
+                                 descending = True, 
+                                 limit = limit+1,
+                                 endKey = endKey, 
                                  skip = 0,
-                                 group=True, group_level=1)
+                                 group=group, group_level=1)
+
+
         else:
           documents = docType.view(view, descending = True, limit = limit)
 
