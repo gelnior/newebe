3df2128a5dab2d4f68bbcf1309a75866c124b898
Julien Miotte <mike.perdide@gmail.com>
2012-09-13 17:31:22 +0200

    [PEP8] Applied PEP8 to news/

--- news/handlers.py
+++ news/handlers.py
@@ -1,7 +1,7 @@
 import logging
 import markdown
 
-from tornado import gen 
+from tornado import gen
 from tornado.web import asynchronous
 from tornado.escape import json_encode
 from couchdbkit.exceptions import ResourceNotFound
@@ -17,8 +17,8 @@ from newebe.core.attach import Converter
 
 logger = logging.getLogger("newebe.news")
 
-# When a new post is created, it is forwarded to contacts via POST requests. 
-# CONTACT_PATH is the end of the URI where data are sent. Full URI is 
+# When a new post is created, it is forwarded to contacts via POST requests.
+# CONTACT_PATH is the end of the URI where data are sent. Full URI is
 # (contact URI + CONTACT_PATH).
 CONTACT_PATH = 'microposts/contacts/'
 
@@ -31,18 +31,16 @@ class NewsSuscribeHandler(NewebeAuthHandler):
     Handler that managers long polling connections.
     '''
 
-
     @asynchronous
     def get(self):
         '''
         Add request to the waiting queue. When a new post will come, this
-        handler callback will be called then a response with new post 
+        handler callback will be called then a response with new post
         will be sent to client that made this request.
         '''
 
         logger.info("Long polling incoming")
         connections.append(self.async_callback(self.on_new_post))
-
 
     def on_new_post(self, response_data):
         '''
@@ -57,46 +55,45 @@ class NewsSuscribeHandler(NewebeAuthHandler):
 class MicropostHandler(NewebeAuthHandler):
     '''
     Manage single post data :
-    * GET request returns post corresponding to the id given in the request URL.
-    * DELETE request deletes post corresponding to the id given in the request 
+    * GET request returns post corresponding to the id given in the request
+    URL.
+    * DELETE request deletes post corresponding to the id given in the request
     URL. Then send the delete request to contacts.
     '''
-    
 
     def get(self, postId):
         '''
-        GET request returns post corresponding to the id given in the request 
+        GET request returns post corresponding to the id given in the request
         URL.
         '''
 
         micropost = MicroPostManager.get_micropost(postId)
         self.return_one_document_or_404(micropost, "No post found")
 
-
     @asynchronous
     def delete(self, id):
         '''
-        Deletes the post of which id is equal to postId, add an activity and 
+        Deletes the post of which id is equal to postId, add an activity and
         forwards the delete request to each trusted contact.
 
-        put instead of delete because tornado does not support body in DELETE 
+        put instead of delete because tornado does not support body in DELETE
         requests...
         '''
 
         micropost = MicroPostManager.get_micropost(id)
 
-        if micropost:            
+        if micropost:
             user = UserManager.getUser()
 
             if micropost.authorKey == user.key:
                 self.create_owner_deletion_activity(
-                    micropost, "deletes", "micropost")  
+                    micropost, "deletes", "micropost")
                 self.send_deletion_to_contacts(CONTACT_PATH, micropost)
             postIndexer = indexer.Indexer()
             postIndexer.remove_doc(micropost)
             micropost.delete()
             self.return_success("Micropost deletion succeeds.")
-            
+
         else:
             self.return_failure("Micropost not found.", 404)
 
@@ -108,13 +105,12 @@ class NewsHandler(NewebeAuthHandler):
     POST : Creates a new microposts and forward the activity to contacts.
     '''
 
-
     @asynchronous
     def get(self, startKey=None, tag=None):
         '''
-        Return microposts by pack of NEWS_LIMIT at JSON format. If a start key 
-        is given in URL (it means a date like 2010-10-05-12-30-48), 
-        microposts from this date are returned. Else latest news are returned. 
+        Return microposts by pack of NEWS_LIMIT at JSON format. If a start key
+        is given in URL (it means a date like 2010-10-05-12-30-48),
+        microposts from this date are returned. Else latest news are returned.
 
         Arguments:
             *startKey* The date from where news should be returned.
@@ -127,12 +123,12 @@ class NewsHandler(NewebeAuthHandler):
         '''
         When post request is received, micropost data are expected as
         a JSON object. It is extracted from it
-        then stored inside a new Microposts object. Micropost author is 
+        then stored inside a new Microposts object. Micropost author is
         automatically set with current user and current date is set as date.
 
         Created microposts are forwarded to contacts.
         '''
-        
+
         logger.info("Micropost post received.")
 
         data = self.get_body_as_dict(expectedFields=["content", "tags"])
@@ -141,35 +137,33 @@ class NewsHandler(NewebeAuthHandler):
             user = UserManager.getUser()
             converter = Converter()
             micropost = MicroPost(
-                authorKey = user.key,
-                author = user.name,
-                content = data['content'],
-                attachments = converter.convert(data),
-                tags = data["tags"]
+                authorKey=user.key,
+                author=user.name,
+                content=data['content'],
+                attachments=converter.convert(data),
+                tags=data["tags"]
             )
             micropost.save()
             postIndexer = indexer.Indexer()
             postIndexer.index_micropost(micropost)
             converter.add_files(micropost)
 
-            self.create_owner_creation_activity(micropost, 
+            self.create_owner_creation_activity(micropost,
                                                 "writes", "micropost")
             self.send_creation_to_contacts(CONTACT_PATH, micropost)
-                          
-            logger.info("Micropost successfuly posted.") 
+
+            logger.info("Micropost successfuly posted.")
             self.return_json(micropost.toJson())
-    
-        else: 
+
+        else:
             self.return_failure(
                     "Sent data were incorrects. No post was created.", 400)
 
-        
 
 class NewsContactHandler(NewebeHandler):
     '''
     This resource allows authorized contacts to send their microposts.
     '''
-
 
     def post(self):
         '''
@@ -193,13 +187,13 @@ class NewsContactHandler(NewebeHandler):
             if contact:
                 if not micropost:
                     micropost = MicroPost(
-                        authorKey = authorKey,
-                        author = data["author"],
-                        content = data['content'],
-                        date = date,
-                        attachments = data.get("attachments", []),
-                        isMine = False,
-                        tags = contact.tags
+                        authorKey=authorKey,
+                        author=data["author"],
+                        content=data['content'],
+                        date=date,
+                        attachments=data.get("attachments", []),
+                        isMine=False,
+                        tags=contact.tags
                     )
                     micropost.save()
 
@@ -207,10 +201,10 @@ class NewsContactHandler(NewebeHandler):
                     postIndexer.index_micropost(micropost)
                     self._notify_suscribers(micropost)
 
-                    self.create_creation_activity(contact, micropost, 
+                    self.create_creation_activity(contact, micropost,
                             "writes", "micropost")
                     self._write_create_log(micropost)
-            
+
                 self.return_json(micropost.toJson(), 201)
 
             else:
@@ -219,7 +213,6 @@ class NewsContactHandler(NewebeHandler):
         else:
             self.return_failure("No data sent.", 405)
 
-
     def _notify_suscribers(self, micropost):
         '''
         Notify suscribed client (long polling requests) that a *micropost*
@@ -227,9 +220,8 @@ class NewsContactHandler(NewebeHandler):
         '''
 
         while connections:
-            connection = connections.pop() 
+            connection = connections.pop()
             connection(micropost.toJson())
-
 
     def _write_create_log(self, micropost):
         '''
@@ -238,10 +230,9 @@ class NewsContactHandler(NewebeHandler):
 
         logger.info("Micropost from %s received" % micropost.author)
 
-
     def put(self):
         '''
-        When a delete request from a contact is incoming, it executes the 
+        When a delete request from a contact is incoming, it executes the
         delete request locally then it creates a new activity corresponding
         to this deletion.
         '''
@@ -271,7 +262,6 @@ class NewsContactHandler(NewebeHandler):
         else:
             self.return_failure("No data sent.", 400)
 
-
     def _write_delete_log(self, micropost):
         '''
         Prints a log telling that an incoming micropost has been saved.
@@ -284,7 +274,7 @@ class MicropostAttachedFileHandler(NewebeAuthHandler):
 
     def get(self, postId, fileName):
         '''
-        Return file which corresponds to *filename* and which is attached to 
+        Return file which corresponds to *filename* and which is attached to
         micropost of which ID is equal to postId.
         '''
 
@@ -302,11 +292,10 @@ class MicropostAttachedFileHandler(NewebeAuthHandler):
 class MicropostDlAttachedFileHandler(NewebeAuthHandler):
     '''
     To not overload bandwidht, post attached file are downloaded on request.
-    This resource allows user to download files attached to micropost 
+    This resource allows user to download files attached to micropost
     which missed.
     '''
 
-    
     @asynchronous
     @gen.engine
     def post(self, postId):
@@ -330,10 +319,10 @@ class MicropostDlAttachedFileHandler(NewebeAuthHandler):
             }
 
             client.post(contact, "microposts/contacts/attach/",
-                        json_encode(body), 
+                        json_encode(body),
                         callback=(yield gen.Callback("getattach")))
             response = yield gen.Wait("getattach")
-            
+
             if response.error:
                 self.return_failure(
                         "An error occured while retrieving picture.")
@@ -358,7 +347,7 @@ class MicropostContactAttachedFileHandler(NewebeHandler):
 
     def post(self):
         '''
-        Returns file which is attached to post corresponding to a given 
+        Returns file which is attached to post corresponding to a given
         date (we assumed a user can't post two posts at the same time).
         Expected data :
         * path : file name
@@ -388,21 +377,20 @@ class MicropostContactAttachedFileHandler(NewebeHandler):
 class MicropostTHandler(NewebeAuthHandler):
     '''
     This handler allows to retrieve micropost at HTML format.
-    * GET: Return for given id the HTML representation of corresponding 
+    * GET: Return for given id the HTML representation of corresponding
            micropost.
     '''
-    
 
     def get(self, postId):
         '''
-        Returns for given id the HTML representation of corresponding 
+        Returns for given id the HTML representation of corresponding
         micropost.
         '''
 
         micropost = MicroPostManager.get_micropost(postId)
         if micropost:
             if micropost.content:
-                 micropost.content = markdown.markdown(micropost.content)
+                micropost.content = markdown.markdown(micropost.content)
 
             self.render("templates/micropost.html", micropost=micropost)
         else:
@@ -410,7 +398,6 @@ class MicropostTHandler(NewebeAuthHandler):
 
 
 class NewsRetryHandler(NewebeAuthHandler):
-
 
     @asynchronous
     def post(self, key):
@@ -421,8 +408,9 @@ class NewsRetryHandler(NewebeAuthHandler):
         '''
 
         micropost = MicroPostManager.get_micropost(key)
-        
-        data = self.get_body_as_dict(expectedFields=["contactId", "activityId"])
+
+        data = self.get_body_as_dict(expectedFields=["contactId",
+                                                     "activityId"])
 
         if micropost and data:
 
@@ -438,28 +426,29 @@ class NewsRetryHandler(NewebeAuthHandler):
                 self.return_failure("Activity not found", 404)
             else:
                 if contact.name:
-                    logger.info("Attempt to resend a post to contact: %s." % contact.name)
+                    info_str = "Attempt to resend a post to contact: %s."
+                    logger.info(info_str % contact.name)
                 self.forward_to_contact(micropost, contact, activity)
         else:
             self.return_failure("Micropost not found", 404)
 
     @asynchronous
     @gen.engine
-    def forward_to_contact(self, micropost, contact, activity, method = "POST"):
+    def forward_to_contact(self, micropost, contact, activity, method="POST"):
         '''
-        *micropost* is sent to *contact* via a request of which method is set 
+        *micropost* is sent to *contact* via a request of which method is set
         as *method*. If request succeeds, error linked to this contact
         is removed. Else nothing is done and error code is returned.
         '''
 
-        httpClient = ContactClient()         
+        httpClient = ContactClient()
         body = micropost.toJson(localized=False)
 
         try:
-            httpClient.post(contact, CONTACT_PATH, body, 
+            httpClient.post(contact, CONTACT_PATH, body,
                             callback=(yield gen.Callback("retry")))
             response = yield gen.Wait("retry")
-              
+
             if response.error:
                 self.return_failure("Posting micropost to contact failed.")
 
@@ -474,18 +463,16 @@ class NewsRetryHandler(NewebeAuthHandler):
         except:
             self.return_failure("Posting micropost to contact failed.")
 
-       
-       
     @asynchronous
     @gen.engine
     def put(self, key):
         '''
-        Resend deletion of micropost with *key* as key to the contact given in 
-        the posted JSON. Corresponding activity ID is given inside the posted 
+        Resend deletion of micropost with *key* as key to the contact given in
+        the posted JSON. Corresponding activity ID is given inside the posted
         json.
         Here is the format : {"contactId":"data","activityId":"data"}
         '''
-        
+
         data = self.get_body_as_dict(
                 expectedFields=["contactId", "activityId", "extra"])
 
@@ -506,22 +493,21 @@ class NewsRetryHandler(NewebeAuthHandler):
 
                 user = UserManager.getUser()
                 micropost = MicroPost(
-                    authorKey = user.key, 
-                    date = date_util.get_date_from_db_date(date)
+                    authorKey=user.key,
+                    date=date_util.get_date_from_db_date(date)
                 )
-                
+
                 logger.info(
                     "Attempt to resend a post deletion to contact: {}.".format(
                         contact.name))
-                httpClient = ContactClient()         
+                httpClient = ContactClient()
                 body = micropost.toJson(localized=False)
 
-
                 try:
-                    httpClient.put(contact, CONTACT_PATH, body, 
+                    httpClient.put(contact, CONTACT_PATH, body,
                                    callback=(yield gen.Callback("retry")))
                     response = yield gen.Wait("retry")
-                  
+
                     if response.error:
                         self.return_failure(
                                 "Deleting micropost to contact failed.")
@@ -537,7 +523,6 @@ class NewsRetryHandler(NewebeAuthHandler):
                 except:
                     self.return_failure("Deleting micropost to contact failed.")
 
-
         else:
             self.return_failure("Micropost not found", 404)
 
@@ -549,12 +534,11 @@ class MyNewsHandler(NewebeAuthHandler):
     GET : Retrieve last 10 microposts published before a given date by owner.
     '''
 
-
     def get(self, startKey=None, tag=None):
         '''
-        Return microposts by pack of NEWS_LIMIT at JSON format. If a start key 
-        is given in URL (it means a date like 2010-10-05-12-30-48), 
-        microposts from this date are returned. Else latest news are returned. 
+        Return microposts by pack of NEWS_LIMIT at JSON format. If a start key
+        is given in URL (it means a date like 2010-10-05-12-30-48),
+        microposts from this date are returned. Else latest news are returned.
         Only microposts published by Newebe owner are returned.
 
         Arguments:
@@ -562,13 +546,12 @@ class MyNewsHandler(NewebeAuthHandler):
         '''
 
         self.return_documents_since(MicroPostManager.get_mine, startKey, tag)
-    
+
 
 class NewsSearchHandler(NewebeAuthHandler):
     """
     Handler that allows user to search through its microposts.
     """
-
 
     def post(self):
         '''
@@ -606,4 +589,3 @@ class NewsTutorial1THandler(NewebeAuthHandler):
 class NewsTutorial2THandler(NewebeAuthHandler):
     def get(self):
         self.render("templates/tutorial_2.html")
-
