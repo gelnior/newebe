3222847523ed83f5c640783cf44e569cd0065b61
Julien Miotte <mike.perdide@gmail.com>
2012-09-14 17:46:08 +0200

    Making tools a module and adding a Python file for dependencies checking

---
+++ newebe/tools/dependencies.py
@@ -0,0 +1,52 @@
+import os
+import socket
+
+
+def is_exe(fpath):
+    return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+
+def which(program):
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
+
+def check_PIL():
+    try:
+        import PIL
+    except ImportError, e:
+        print "Newebe needs python-imaging."
+
+
+def check_CouchDB():
+    test_socket = socket.socket()
+    try:
+        test_socket.connect(("127.0.0.1", 5984))
+    except socket.error, e:
+        print "Newebe needs CouchDB, but nothing is listening on 5984"
+
+
+def check_OpenSSL():
+    if not which("openssl"):
+        print "Newebe needs openssl."
+
+
+def check_curl_config():
+    if not which("curl-config"):
+        print "Newebe needs curl-config."
+
+
+def check_all_dependencies():
+    check_curl_config()
+    check_PIL()
+    check_CouchDB()
+    check_OpenSSL()
