a7a611363367fa26c6ac85c882d99aee5437b244
Gelnior <gelnior@free.fr>
2013-12-23 01:36:21 +0100

    remove credits

--- newebe/config.py
+++ newebe/config.py
@@ -1,16 +1,3 @@
-# * File Name : config.py
-#
-# * Copyright (C) 2010 Gaston TJEBBES <g.t@majerti.fr>
-# * Company : Majerti ( http://www.majerti.fr )
-#
-#   This software is distributed under GPLV3
-#   License: http://www.gnu.org/licenses/gpl-3.0.txt
-#
-# * Creation Date : 15-09-2012
-# * Last Modified :
-#
-# * Project : Newebe
-#
 """
     Provide a main config object to uniformize configuration access
 """
