2b0a7f992db64d44853ad29f41a686c89e49b1fd
Frank <frank.rousseau@free.fr>
2014-04-22 22:21:32 +0200

    clean fabfile

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -1,18 +1,19 @@
+import random
+import string
+
 from fabric.api import sudo, cd, task, prompt
 from fabric.contrib import files
 from fabtools import require, python, supervisor
 
-# Variables
-newebe_dir = "/home/newebe/newebe"
+# Constants
+newebe_dir = "/usr/local/var/newebe"
+newebe_log_dir = "/usr/local/var/log/newebe"
+newebe_conf_dir = "/usr/local/etc/newebe"
+newebe_user_dir = "/usr/local/var/newebe"
 newebe_process = newebe_user = "newebe"
-newebe_user_dir = "/home/newebe/"
-python_exe = newebe_dir + "/virtualenv/bin/python"
 newebe_exe = "newebe_server.py"
 
 # Helpers
-import random
-import string
- 
 def random_string(n):
     """Create n length random string"""
 
@@ -20,16 +21,19 @@ def random_string(n):
     code = ''.join([random.choice(chars) for i in range(n)])
     return code
 
+
 def newebedo(cmd):
     """Run a commande as a newebe user"""
 
     sudo(cmd, user=newebe_user)
+
 
 def delete_if_exists(filename):
     """Delete given file if it already exists"""
 
     if files.exists(filename):
         newebedo("rm -rf %s" % filename)
+
 
 # Install tasks
 
@@ -43,8 +47,8 @@ def setup():
     make_dirs()
     build_configuration_file()
     build_certificates()
-    setup_supervisord()
     set_supervisord_config()
+
 
 @task()
 def install_deb_packages():
@@ -62,9 +66,11 @@ def install_deb_packages():
         'git',
         'libxml2-dev',
         'libxslt-dev',
+        'supervisor',
         'openssl'
     ])
     #sudo("apt-get build-dep python-imaging")
+
 
 @task()
 def create_user():
@@ -72,20 +78,25 @@ def create_user():
 
     require.user(newebe_user, newebe_user_dir)
 
+
 @task()
 def install_newebe():
     """Install Newebe as a main software"""
 
     update_source()
-    
+
+
 @task()
 def make_dirs():
     """Make dir required for a proper install"""
 
-    with cd(newebe_user_dir):
-        delete_if_exists('newebe')
-        newebedo("mkdir newebe")
-        newebedo("mkdir newebe/certs")
+    sudo('mkdir -p %s' % newebe_conf_dir)
+    sudo('mkdir -p %s' % newebe_dir)
+    sudo('mkdir -p %s' % newebe_log_dir)
+    sudo('chown newebe:newebe %s' % newebe_dir)
+    sudo('chown newebe:newebe %s' % newebe_log_dir)
+    sudo('chown newebe:newebe %s' % newebe_conf_dir)
+
 
 @task()
 def build_configuration_file():
@@ -93,35 +104,41 @@ def build_configuration_file():
 
     timezone = prompt("""
 Which time zone do you want for your database (default is Europe/Paris,
-Check Newebe wiki for timezone list) ? 
+Check Newebe wiki for timezone list) ?
 \n \n
 """)
     if not timezone:
         timezone = "Europe/Paris"
 
-    with cd(newebe_dir):
+    with cd(newebe_conf_dir):
         delete_if_exists('local_settings.py')
         newebedo('echo "main:" >> config.yaml')
-        newebedo('echo "    port: 8000" >> config.yaml')
+        newebedo('echo "    port: 8282" >> config.yaml')
         newebedo('echo "    debug: False" >> config.yaml')
-        newebedo("echo '    timezone: \"%s\"' >> config.yaml" % timezone)
+        newebedo('echo "    timezone: \"%s\" >> config.yaml"' % timezone)
+        newebedo('echo "    path: \"%s\" >> config.yaml"' % newebe_dir)
+        newebedo('echo "    logpath: \"%s\" >> config.yaml"' % newebe_log_dir)
         newebedo('echo "db:" >> config.yaml')
         newebedo("echo '    name: \"newebe\"' >> config.yaml")
         newebedo('echo "security:" >> config.yaml')
+        newebedo('echo "    certificate: "%s/certs/server.crt"' % newebe_conf_dir)
+        newebedo('echo "    private_key: "%s/certs/server.key"' % newebe_conf_dir)
         newebedo("echo '    cookie_key: \"%s\"' >> config.yaml" % \
                  random_string(42))
+
 
 @task()
 def build_certificates():
     """Build HTTPS certificates"""
 
-    with cd(newebe_dir + "/certs"):
+    with cd(newebe_conf_dir + "/certs"):
         delete_if_exists('server.key')
         delete_if_exists('server.crt')
         newebedo("openssl genrsa -out ./server.key 1024")
         newebedo("openssl req -new -x509 -days 3650 -key ./server.key -out\
                   ./server.crt -batch")
-    
+
+
 @task()
 def setup_supervisord():
     """Install python daemon manager, supervisord"""
@@ -129,18 +146,21 @@ def setup_supervisord():
     python.install("meld3==0.6.9", use_sudo=True)
     require.deb.package("supervisor")
 
+
 @task()
 def set_supervisord_config():
     """Configure Newebe runner for supervisord"""
 
     require.supervisor.process(newebe_process,
-        command='%s --configfile=%s' % 
+        command='%s --configfile=%s' %
             (newebe_exe, newebe_dir + "/config.yaml"),
         user=newebe_user
     )
     supervisor.start_process(newebe_process)
 
+
 # Update tasks
+
 
 @task()
 def update():
@@ -148,12 +168,14 @@ def update():
 
     update_source()
     restart_newebe()
-    
+
+
 @task()
 def update_source():
     """Simple git pull inside newebe directory"""
 
     python.install("git+git://github.com/gelnior/newebe.git")
+
 
 @task()
 def restart_newebe():
